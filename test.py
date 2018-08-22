955363c5590b1091fc426ac138a25dd90c51d7e1
sk.moon <sk.moon@navercorp.com>
2018-08-22 10:06:09 +0900

    first

---
+++ test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+tensor = torch.rand(2,2,)
+x = Variable(tensor, requires_grad=True)
+
+print tensor
+print x
+
+test_list = ['aa', 'bb', 'cc']
+
+for i in test_list:
+    print(i)
+
+
+
